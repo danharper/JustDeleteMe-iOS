f2de415a7f161d60b2e1b599ee75ceafae03708d
Dan Harper <intouch@danharper.me>
2014-06-04 00:13:14 +0100

    Initial Commit

---
+++ JustDeleteMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JustDeleteMe
+//
+//  Created by Dan Harper on 04/06/2014.
+//  Copyright (c) 2014 Dan Harper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
